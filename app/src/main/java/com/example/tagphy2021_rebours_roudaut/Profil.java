f4be853b16569f4b63803fa5a7fdc23c5affe4db
EnolaRdt <roudautenola@gmail.com>
2021-09-21 16:26:42 +0200

    NewActivity Profil

--- app/src/main/java/com/example/tagphy2021_rebours_roudaut/Profil.java
+++ app/src/main/java/com/example/tagphy2021_rebours_roudaut/Profil.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Profil extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.moncoeur);
+        setContentView(R.layout.activity_profil);
     }
 }
