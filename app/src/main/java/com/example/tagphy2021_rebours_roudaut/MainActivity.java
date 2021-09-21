4dd4f367e7ee94510a603048aff744de82e6ceb8
EnolaRdt <roudautenola@gmail.com>
2021-09-21 17:43:40 +0200

    Activity_main color

--- app/src/main/java/com/example/tagphy2021_rebours_roudaut/MainActivity.java
+++ app/src/main/java/com/example/tagphy2021_rebours_roudaut/MainActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "QuizzProject";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.moncoeur);
+        setContentView(R.layout.activity_main);
     }
 }
